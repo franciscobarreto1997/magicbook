8356424bff52298b34ad4671538f25e5c3a04848
francisco <franciscoantunesbarreto1997@gmail.com>
2020-03-16 14:45:58 +0000

    fully responsive, added carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,16 @@ import changeNavbarColor from '../functions/change_navbar_color'
 import scrollDownOnClick from '../functions/scroll_down_on_click'
 import fetchAlbumsOnHover from '../functions/fetch_albums_on_hover'
 import responsiveNavbar from '../functions/responsive_navbar'
+import addCarouselForMobile from '../functions/add_carousel_for_mobile'
 
 changeNavbarColor()
 scrollDownOnClick()
 fetchAlbumsOnHover()
 responsiveNavbar()
+addCarouselForMobile()
+
+
+window.addEventListener("load", () => {
+  $('.carousel').carousel('pause')
+});
+
